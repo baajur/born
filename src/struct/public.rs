88aed303a62ea102778a7bed1cd85d9297c2075f
Steadylearner <32325099+steadylearner@users.noreply.github.com>
2020-09-11 07:08:03 -0400

    Update public.rs
    #1 ? instead of * for the better error message.

--- src/struct/public.rs
+++ src/struct/public.rs
@@ -24,7 +24,7 @@
 /// ```
 #[macro_export]
 macro_rules! public_struct {
-    (pub struct $commonstruct:ident { $( $commonfieldpub:vis $commonfield:ident: $commonty:ty ),+ $(,)* }) => {
+    (pub struct $commonstruct:ident { $( $commonfieldpub:vis $commonfield:ident: $commonty:ty ),+ $(,)? }) => {
         nested_macro! {
             ($s:tt) => {
                 macro_rules! $commonstruct {
