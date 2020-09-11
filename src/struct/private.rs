a8a93141a99834d5afaccb227612d0817c4d4c0f
Steadylearner <32325099+steadylearner@users.noreply.github.com>
2020-09-11 07:07:17 -0400

    Update private.rs
    #1 ? instead of * for the better error message.

--- src/struct/private.rs
+++ src/struct/private.rs
@@ -20,7 +20,7 @@
 /// ```
 #[macro_export]
 macro_rules! private_struct {
-    (struct $commonstruct:ident { $( $commonfield:ident: $commonty:ty ),+ $(,)* }) => {
+    (struct $commonstruct:ident { $( $commonfield:ident: $commonty:ty ),+ $(,)? }) => {
         nested_macro! {
             ($s:tt) => {
                 macro_rules! $commonstruct {
